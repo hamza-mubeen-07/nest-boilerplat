5e55f87543a7744c5d8547c7182f954f776f9cbe
Fahad Khan <fahad@rebeltechnology.io>
2021-11-08 14:44:49 +0500

    NJT-11 - Dotenv package for loading env

--- apps/user/src/user.constants.ts
+++ apps/user/src/user.constants.ts
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv';
+dotenv.config({ path: __dirname + '/./../../../.env' });
+
 export const ORIGIN_SITE = process.env.ORIGIN_SITE || '';
 
 export const ORIGIN_DASHBOARD = process.env.ORIGIN_DASHBOARD || '';
@@ -5,3 +8,7 @@ export const ORIGIN_DASHBOARD = process.env.ORIGIN_DASHBOARD || '';
 export const REQUEST_BODY_SIZE = '50mb';
 
 export const SERVER_PORT = process.env.USER_PORT;
+
+// INFO: ONLY FOR EXAMPLE, DELETE IT FOR CLONED PROJECTS!
+export const COUNTRY_LIST_API_ENDPOINT = '';
+export const SINGLE_COUNTRY_API_ENDPOINT = '';
