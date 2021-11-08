f20dca7f162dccd95e9961f233b08b96d35ca016
Fahad Khan <fahad@rebeltechnology.io>
2021-11-08 18:21:38 +0500

    NJT-12 - User endpoints.

--- apps/user/src/user.module.ts
+++ apps/user/src/user.module.ts
@@ -2,11 +2,12 @@ import { Module, HttpModule } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { PrismaService } from '../../../prisma/prisma.service';
+import { UserRepository } from '../../../prisma/repositories/user.repository';
 
 @Module({
   imports: [HttpModule],
   controllers: [UserController],
-  providers: [UserService, PrismaService],
+  providers: [UserService, PrismaService, UserRepository],
   exports: [UserService],
 })
 export class UserModule {}
