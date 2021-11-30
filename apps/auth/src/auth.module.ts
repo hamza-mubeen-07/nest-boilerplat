dfb2d684638415ad236b6d602e637c2cdef8d37f
Fahad Khan <fahad@rebeltechnology.io>
2021-11-30 15:43:32 +0500

    NJT-16 - Added Nats example.

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,9 +1,22 @@
-import { HttpModule, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { ClientsModule } from '@nestjs/microservices';
+import { APP_NATS_QUEUE, MS_INTERNAL_PORT, NATS_URL } from './auth.constants';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'NATS_SERVICE',
+        options: {
+          url: NATS_URL,
+          queue: APP_NATS_QUEUE,
+          port: MS_INTERNAL_PORT,
+        },
+      },
+    ]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
