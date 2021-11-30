eaca4bc82a05677b94486f00983b65d18ff2c777
Fahad Khan <fahad@rebeltechnology.io>
2021-11-30 15:43:48 +0500

    NJT-16 - Generic response and exception classes.

--- common/interceptors/generic-exception.interceptor.ts
+++ common/interceptors/generic-exception.interceptor.ts
@@ -5,7 +5,6 @@ import {
   HttpException,
   HttpStatus,
 } from '@nestjs/common';
-
 // INFO: Don't remove this, will be used later
 // import * as Sentry from '@sentry/minimal';
 
@@ -13,31 +12,38 @@ import {
  * Handles all exceptions
  */
 @Catch()
-export class GlobalExceptionsFilter implements ExceptionFilter {
+export class GenericExceptionFilter implements ExceptionFilter {
   catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
 
     const status =
-      exception instanceof HttpException
-        ? exception.getStatus()
-        : HttpStatus.INTERNAL_SERVER_ERROR;
-
-    if (status >= 500) {
       // INFO: Don't remove this, will be used later
       // Sentry.captureException(exception, {
       //   extra: {
       //     url: request.url,
       //   },
       // });
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    if (status >= HttpStatus.INTERNAL_SERVER_ERROR) {
       console.error(exception);
       response.status(status).json({
+        response: null,
+        error: true,
         message:
           'An unknown error has occurred, we will be resolving it shortly.',
       });
     } else {
       response.status(status).json({
-        message: exception instanceof Error ? exception.message : '',
+        response: null,
+        error: true,
+        message:
+          exception instanceof Error
+            ? exception.message
+            : 'Unknown client error!',
       });
     }
   }
