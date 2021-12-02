da29592bcb4b6fe02b550dc638c6532ecdf4bf56
Fahad Khan <fahad@rebeltechnology.io>
2021-12-02 17:39:18 +0500

    NJT-18 - Example page setup.

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,8 +1,16 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Inject,
+  MessageEvent,
+  Sse,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
 import { AuthService } from './auth.service';
 import { EventPattern, ClientProxy } from '@nestjs/microservices';
 import { HttpService } from '@nestjs/axios';
-import { firstValueFrom } from 'rxjs';
+import { firstValueFrom, Observable, interval, map } from 'rxjs';
 
 @Controller('auth')
 export class AuthController {
@@ -40,5 +48,20 @@ export class AuthController {
 
   testApiCall(url) {
     return firstValueFrom(this.httpService.get(url)).catch(() => null);
+  }
+
+  /**
+   * Example page for SSR.
+   */
+  @Get('ssr_example')
+  root(@Res() res: Response) {
+    return res.render('admin/page-one', {
+      message: 'SSR, SSE and WS example',
+    });
+  }
+
+  @Sse('sse')
+  sse(): Observable<MessageEvent> {
+    return interval(1000).pipe(map((_) => ({ data: { hello: 'world' } })));
   }
 }
