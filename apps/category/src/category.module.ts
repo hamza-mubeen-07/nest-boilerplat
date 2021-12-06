f52aabe9c1a81acd9a3b2f8f021cbb618d501f38
Fahad Khan <fahad@rebeltechnology.io>
2021-12-06 16:59:15 +0500

    NJT-20 - Cache config and setup.

--- apps/category/src/category.module.ts
+++ apps/category/src/category.module.ts
@@ -1,11 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
 import { CategoryController } from './category.controller';
 import { CategoryService } from './category.service';
 import { PrismaService } from '../../../prisma/prisma.service';
 import { CategoryRepository } from '../../../prisma/repositories/category.repository';
+import { MAX_CACHE_ITEMS, MAX_CACHE_TIME_SEC } from './category.constants';
 
 @Module({
-  imports: [],
+  imports: [CacheModule.register({
+    ttl: MAX_CACHE_TIME_SEC,
+    max: MAX_CACHE_ITEMS,
+  })],
   controllers: [CategoryController],
   providers: [CategoryService, PrismaService, CategoryRepository],
   exports: [CategoryService],
